a10fe87f00b5157dcd4fb8409dc5d6c523ac37f0
acaleza <adrian.calvo1@gmail.com>
2023-02-27 00:25:37 +0100

    Añadido patrón builder

--- src/tests/diseaseFilter.test.ts
+++ src/tests/diseaseFilter.test.ts
@@ -1,4 +1,4 @@
-import { DiseaseFilter } from '../core/diseaseFilter';
+import {Case, Diagnosis, DiseaseFilter} from '../core/diseaseFilter';
 
 function createDiagnosis(id: number, location: string) {
     return {
@@ -29,19 +29,16 @@ describe('Disease filter', () => {
     it('filters cases when several diagnosis filters are applied together', () => {
         const patientName1 = 'Chupito';
         const patientName2 = 'Juliana';
-        cases = [
-            createCase(1, patientName1),
-            createCase(2, patientName2),
-            createCase(3, 'Irrelevant-name'),
-        ];
         const searchCriteria1 = 'Vías respiratorias altas';
         const searchCriteria2 = 'Cerebro';
-        diagnoses = [
-            createDiagnosis(1, searchCriteria1),
-            createDiagnosis(2, searchCriteria2),
-            createDiagnosis(3, 'Irrelevant-location')
-        ];
-        const diseaseFilter = DiseaseFilter.create(cases, diagnoses);
+
+        const fixtures = casesWithDiagnoses()
+            .havingDiagnosisWithLocationAndCaseWithName(searchCriteria1, patientName1)
+            .havingDiagnosisWithLocationAndCaseWithName(searchCriteria2, patientName2)
+            .havingDiagnosisWithLocationAndCaseWithName('Irrelevant-location', 'Irrelevant-name')
+            .build();
+
+        const diseaseFilter = DiseaseFilter.create(fixtures.cases(), fixtures.diagnoses());
         diseaseFilter.addFilter(searchCriteria2);
         diseaseFilter.addFilter(searchCriteria1);
 
@@ -51,4 +48,29 @@ describe('Disease filter', () => {
         expect(result[1].patientName).toBe(patientName1);
         expect(result[0].patientName).toBe(patientName2);
     });
+
+    function casesWithDiagnoses() {
+        let diagnosisId = 0;
+        let diagnoses: Diagnosis[] = [];
+        let cases: Case[] = [];
+
+        const add = (location: string, patientName: string) => {
+            diagnosisId++;
+            diagnoses.push(createDiagnosis(diagnosisId, location));
+            cases.push(createCase(diagnosisId, patientName));
+        };
+
+        const builder = {
+            havingDiagnosisWithLocationAndCaseWithName: (location: string, patientName: string) => {
+                add(location, patientName);
+                return builder;
+            },
+            build: () => ({
+                cases: () => cases,
+                diagnoses: () => diagnoses,
+            }),
+        };
+
+        return builder;
+    }
 });
